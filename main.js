c9045d245a1f8623ca60aed18e3f731d7b71874e
Xavier-says-hi-school <s2226478@online.houstonisd.org>
2025-05-29 11:45:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 function start() {
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+      const canvas = document.getElementById("myCanvas");
+      const ctx = canvas.getContext("2d");
 
-// Create gradient
-var grd = ctx.createRadialGradient(75,50,5,90,60,100);
-grd.addColorStop(0,"red");
-grd.addColorStop(1,"white");
+      // Create gradient
+      var grd = ctx.createRadialGradient(75,50,5,90,60,100);
+      grd.addColorStop(0,"red");
+      grd.addColorStop(1,"white");
       
-// Fill with gradient
-ctx.fillStyle = grd;
-ctx.fillRect(10,10,150,80);
+      // Fill with gradient
+      ctx.fillStyle = grd;
+      ctx.fillRect(10,10,150,80);
+}
+
+function clear() {
+      const canvas = document.getElementById("myCanvas");
+      const ctx = canvas.getContext("2d");
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(20, 20, 150, 100);
+
 }
